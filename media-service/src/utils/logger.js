1f220f613556f990d1e653bc5c9e2961d5d268f6
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-12-10 21:02:05 +0530

    create the media model + cloudinary config

--- media-service/src/utils/logger.js
+++ media-service/src/utils/logger.js
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
         winston.format.errors({ stack: true }),
         winston.format.json()
     ),
-    defaultMeta: { service: 'media-service' },   
+    defaultMeta: { service: 'post-service' },   
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
